8b0b52478c067d0c501677268de6329b71004d78
supremehappy <supremehappy@aegisep.com>
2023-02-14 09:53:45 +0900

    IntelliJ Add Method
    IntelliJ Add Method

--- Hello/src/main/java/com/simple/Hello/Hello.java
+++ Hello/src/main/java/com/simple/Hello/Hello.java
@@ -11,9 +11,16 @@ public class Hello {
     private static final Logger log = LoggerFactory.getLogger(Hello.class);
 
     @GetMapping("/hello")
-    public String Hello(){
+    public String hello(){
         log.info("하와와와, Hello Get RestAPI인거예요");
         return "Hello World";
+    }
+
+    // new RestAPI Method by IntelliJ
+    @GetMapping("/intelliJ")
+    public String intelliJ(){
+        log.info("하와와와, intelliJ에서 추가한 Get RestAPI인거예요");
+        return "Hello World, intelliJ";
     }
 
 }
